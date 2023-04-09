b185eece83d937186c1970db0b92d5074449b57f
linharesrocha <guilhermelinharesr@gmail.com>
2023-04-09 20:23:47 -0300

    .vscode

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
         'name': 'Guilherme Linhares'
     })
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-def sobre(request):
-    return HttpResponse('SOBRE')
